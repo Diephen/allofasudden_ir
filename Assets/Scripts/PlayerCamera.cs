4958807023665a0ac23583c6754f6b503c53c06b
Ziilii <myliiiziii@gmail.com>
2023-04-01 18:54:25 -0700

    Zi -Add camera setup

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public bool IsOn = false;
+    //[SerializeField] RealityPlayer RealityPlayer;
+
+    public void SetState(bool state)
+    {
+        GetComponent<Camera>().enabled = state;
+    }
+
+    public float Sensitivity
+    {
+        get { return sensitivity; }
+        set { sensitivity = value; }
+    }
+    [Range(0.1f, 9f)] [SerializeField] float sensitivity = 2f;
+    [Tooltip("Limits vertical camera rotation. Prevents the flipping that happens when rotation goes above 90.")]
+    [Range(0f, 90f)] [SerializeField] float yRotationLimit = 88f;
+    //[Range(0f, 180f)] [SerializeField] float xRotationLimit = 88f;
+
+    Vector2 rotation = Vector2.zero;
+    const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
+    const string yAxis = "Mouse Y";
+
+    void Update()
+    {
+        if (!IsOn)
+        {
+            return;
+        }
+
+        rotation.x += Input.GetAxis(xAxis) * sensitivity;
+        rotation.y += Input.GetAxis(yAxis) * sensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+        //rotation.x = Mathf.Clamp(rotation.x, -xRotationLimit, xRotationLimit);
+        var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+        var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+
+        transform.localRotation = xQuat * yQuat; //Quaternions seem to rotate more consistently than EulerAngles. Sensitivity seemed to change slightly at certain degrees using Euler. transform.localEulerAngles = new Vector3(-rotation.y, rotation.x, 0);
+
+        Ray ray = GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            //RealityPlayer.UpdateLookAtTarget(hit.collider.gameObject);
+        }
+        else
+        {
+            //RealityPlayer.UpdateLookAtTarget(null);
+        }
+    }
+}
